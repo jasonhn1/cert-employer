08ac170aa2a4a2bd0426caf955f7cfa4de23bdad
Jason8830 <38028358+Jason8830@users.noreply.github.com>
2022-11-07 16:26:47 -0800

    Formated the dashboard

--- src/App.vue
+++ src/App.vue
@@ -79,17 +79,13 @@ export default {
                             </a>
                         </li>
 
-
                         <li>
                           <a  href="calendar.html" class="waves-effect"><i class="icon-calendar"></i><span> Expired </span></a>
                       </li>
 
                         <li>
                             <a  href="calendar.html" class="waves-effect"><i class="icon-paper-sheet"></i><span>Analytics </span></a>
-                        </li>
-
-
-                       
+                        </li>                       
                     </ul>
 
                 </div>
@@ -105,6 +101,10 @@ export default {
         <!-- ============================================================== -->
         <!-- Start right Content here -->
         <!-- ============================================================== -->
+
+
+
+
         <div class="content-page">
             <!-- Start content -->
             <div class="content">
@@ -112,7 +112,7 @@ export default {
                     <div class="page-title-box">
                         <div class="row align-items-center">
                             <div class="col-sm-6">
-                                <h4 class="page-title">Dashboard nuts</h4>
+                                <h4 class="page-title">Dashboard</h4>
                             </div>
                             <div class="col-sm-6">
                                 <ol class="breadcrumb float-right">
@@ -125,6 +125,106 @@ export default {
                     </div>
                     <!-- end page-title -->
 
+
+                    <!-- START ROW -->
+                    <div class="row">
+                      <div class="col-xl-12">
+                          <div class="card m-b-30">
+                              <div class="card-body">
+                                  <h4 class="mt-0 header-title mb-4">Candidate Links</h4>
+                                  <div class="table-responsive">
+                                      <table class="table table-hover">
+                                          <thead>
+                                              <tr>
+                                                  <th scope="col">Name</th>
+                                                  <th scope="col">Status</th>
+                                                  <th scope="col">Location</th>
+                                                  <th scope="col" colspan="2">Date</th>
+
+                                              </tr>
+                                          </thead>
+                                          <tbody>
+                                              <tr>
+                                                  <td>Jason Nguyen</td>
+                                                  <td><span class="badge badge-success">Ready</span></td>
+                                       
+                                                  <td>USA</td>
+                                                  <td>12/1/2021</td>
+
+                                                  <td>
+                                                      <div>
+                                                          <a href="#" class="btn btn-primary btn-sm ">View</a>
+                                                      </div>
+                                                  </td>
+                                              </tr>
+  
+                                              <tr>
+                                                  <td>Tim Chen</td>
+                                                  <td><span class="badge badge-success">Ready</span></td>
+                                    
+                                                  <td>USA</td>
+                                                  <td>1/1/2021</td>
+
+                                                  <td>
+                                                      <div>
+                                                          <a href="#" class="btn btn-primary btn-sm">View</a>
+                                                      </div>
+                                                  </td>
+                                              </tr>
+                                              
+                                              <tr>
+                                                  <td>John Doe</td>
+                                                  <td><span class="badge badge-success">Ready</span></td>
+                                                  <td>USA</td>
+                                                  <td>1/1/2021</td>
+
+                                                  <td>
+                                                      <div>
+                                                          <a href="#" class="btn btn-primary btn-sm">View</a>
+                                                      </div>
+                                                  </td>
+                                              </tr>
+                                              <tr>
+                                                <td>Ben Mueller</td>
+                                                <td><span class="badge badge-danger">Expired</span></td>
+                                                
+                    
+                                                <td>USA</td>
+                                                <td>1/1/2021</td>
+
+                                                <td>
+                                                    <div>
+                                                        <a href="#" class="btn btn-primary btn-sm disabled">View</a>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                              <td>Jeffery Hover</td>
+                                              <td><span class="badge badge-warning ">Waiting</span></td>
+                                              
+                                
+                                              <td>USA</td>
+                                              <td>12/12/2021</td>
+
+                                              <td>
+                                                  <div>
+                                                      <a href="#" class="btn btn-primary btn-sm disabled">View</a>
+                                                  </div>
+                                              </td>
+                                          </tr>
+                                          </tbody>
+                                      </table>
+                                  </div>
+
+                              </div>
+                          </div>
+                      </div>
+
+                  </div>
+                  <!-- END ROW -->
+
+
+
                     <div class="row">
 
                         <div class="col-sm-6 col-xl-3">
@@ -134,34 +234,17 @@ export default {
                                         <i class="mdi mdi-cube-outline bg-primary  text-white"></i>
                                     </div>
                                     <div>
-                                        <h5 class="font-16">Active Sessions</h5>
+                                        <h5 class="font-16">Active Links</h5>
                                     </div>
                                     <h3 class="mt-4">43,225</h3>
                                     <div class="progress mt-4" style="height: 4px;">
                                         <div class="progress-bar bg-primary" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
-                                    <p class="text-muted mt-2 mb-0">Previous period<span class="float-right">75%</span></p>
+                                    <p class="text-muted mt-2 mb-0">Viewed<span class="float-right">75%</span></p>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="col-sm-6 col-xl-3">
-                            <div class="card">
-                                <div class="card-heading p-4">
-                                    <div class="mini-stat-icon float-right">
-                                        <i class="mdi mdi-briefcase-check bg-success text-white"></i>
-                                    </div>
-                                    <div>
-                                        <h5 class="font-16">Total Revenue</h5>
-                                    </div>
-                                    <h3 class="mt-4">$73,265</h3>
-                                    <div class="progress mt-4" style="height: 4px;">
-                                        <div class="progress-bar bg-success" role="progressbar" style="width: 88%" aria-valuenow="88" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <p class="text-muted mt-2 mb-0">Previous period<span class="float-right">88%</span></p>
-                                </div>
-                            </div>
-                        </div>
 
                         <div class="col-sm-6 col-xl-3">
                             <div class="card">
@@ -170,13 +253,10 @@ export default {
                                         <i class="mdi mdi-tag-text-outline bg-warning text-white"></i>
                                     </div>
                                     <div>
-                                        <h5 class="font-16">Average Price</h5>
+                                        <h5 class="font-16">Read Links</h5>
                                     </div>
                                     <h3 class="mt-4">447</h3>
-                                    <div class="progress mt-4" style="height: 4px;">
-                                        <div class="progress-bar bg-warning" role="progressbar" style="width: 68%" aria-valuenow="68" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <p class="text-muted mt-2 mb-0">Previous period<span class="float-right">68%</span></p>
+
                                 </div>
                             </div>
                         </div>
@@ -188,140 +268,16 @@ export default {
                                         <i class="mdi mdi-buffer bg-danger text-white"></i>
                                     </div>
                                     <div>
-                                        <h5 class="font-16">Add to Card</h5>
+                                        <h5 class="font-16">Expired Links</h5>
                                     </div>
-                                    <h3 class="mt-4">86%</h3>
-                                    <div class="progress mt-4" style="height: 4px;">
-                                        <div class="progress-bar bg-danger" role="progressbar" style="width: 82%" aria-valuenow="82" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <p class="text-muted mt-2 mb-0">Previous period<span class="float-right">82%</span></p>
+                                    <h3 class="mt-4">84</h3>
+                                   
                                 </div>
                             </div>
                         </div>
 
                     </div>
 
-
-                    <!-- START ROW -->
-                    <div class="row">
-                        <div class="col-xl-12">
-                            <div class="card m-b-30">
-                                <div class="card-body">
-                                    <h4 class="mt-0 header-title mb-4">Active Deals</h4>
-                                    <div class="table-responsive">
-                                        <table class="table table-hover">
-                                            <thead>
-                                                <tr>
-                                                    <th scope="col">Name</th>
-                                                    <th scope="col">Status</th>
-                                                    <th scope="col">Amount</th>
-                                                    <th scope="col">Contact</th>
-                                                    <th scope="col">Location</th>
-                                                    <th scope="col" colspan="2">Date</th>
-
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>Jassa</td>
-                                                    <td><span class="badge badge-success">Delivered</span></td>
-                                                    <td>$9,420,000</td>
-                                                    <td>
-                                                        <div>
-                                                            <img src="assets/images/user-2.jpg" alt="" class="thumb-md rounded-circle mr-2"> Jassa
-                                                        </div>
-                                                    </td>
-                                                    <td>Ludhiana</td>
-                                                    <td>15/1/2021</td>
-
-                                                    <td>
-                                                        <div>
-                                                            <a href="#" class="btn btn-primary btn-sm">Edit</a>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Jassa</td>
-                                                    <td><span class="badge badge-warning">Pending</span></td>
-                                                    <td>$3,120,000</td>
-                                                    <td>
-                                                        <div>
-                                                            <img src="assets/images/user-3.jpg" alt="" class="thumb-md rounded-circle mr-2"> Jassa
-                                                        </div>
-                                                    </td>
-                                                    <td>Ludhiana</td>
-                                                    <td>16/1/2021</td>
-
-                                                    <td>
-                                                        <div>
-                                                            <a href="#" class="btn btn-primary btn-sm">Edit</a>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Jassa</td>
-                                                    <td><span class="badge badge-success">Delivered</span></td>
-                                                    <td>$6,360,000</td>
-                                                    <td>
-                                                        <div>
-                                                            <img src="assets/images/user-4.jpg" alt="" class="thumb-md rounded-circle mr-2"> Jassa
-                                                        </div>
-                                                    </td>
-                                                    <td>Ludhiana</td>
-                                                    <td>17/1/2021</td>
-
-                                                    <td>
-                                                        <div>
-                                                            <a href="#" class="btn btn-primary btn-sm">Edit</a>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Jassa</td>
-                                                    <td><span class="badge badge-danger">Cancel</span></td>
-                                                    <td>$5,200,000</td>
-                                                    <td>
-                                                        <div>
-                                                            <img src="assets/images/user-5.jpg" alt="" class="thumb-md rounded-circle mr-2"> Jassa
-                                                        </div>
-                                                    </td>
-                                                    <td>Ludhiana</td>
-                                                    <td>18/1/2021</td>
-
-                                                    <td>
-                                                        <div>
-                                                            <a href="#" class="btn btn-primary btn-sm">Edit</a>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Jassa</td>
-                                                    <td><span class="badge badge-success">Delivered</span></td>
-                                                    <td>$7,250,000</td>
-                                                    <td>
-                                                        <div>
-                                                            <img src="assets/images/user-6.jpg" alt="" class="thumb-md rounded-circle mr-2"> Jassa
-                                                        </div>
-                                                    </td>
-                                                    <td>Ludhiana</td>
-                                                    <td>19/1/2021</td>
-
-                                                    <td>
-                                                        <div>
-                                                            <a href="#" class="btn btn-primary btn-sm">Edit</a>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                    <!-- END ROW -->
 
                 </div>
                 <!-- container-fluid -->
---
+++ src/views/Expired.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Expired</h1>
+</template>
